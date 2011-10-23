ee456240118631977083f98be32865b9639ceef6
Eric Gregory <mrericsir@gmail.com>
2011-10-22 17:06:18 -0700

    fixed file path in dr. pentris

--- demos/dr-pentris/main.lua
+++ demos/dr-pentris/main.lua
@@ -26,7 +26,7 @@ function love.load()
 
 	-- Modipulate
 	modipulate.load()
-	modipulate.open_file('media/v-cf.it')
+	modipulate.open_file('../media/v-cf.it')
 	modipulate.set_playing(true)
 	modipulate.set_volume(1)
 	modipulate.set_on_row_changed(exe_row)
